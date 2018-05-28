4cb1390d933674da864985fcb0ec3911857a40f3
Марко Ёвческий <marko.jovceski@gmail.com>
2018-05-28 23:36:52 +0200

    Reworked project for singular purpose algorithm

---
+++ Fault_Line_Algorithm/MainWindow.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Fault_Line_Algorithm
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.btnGenerateMap = new System.Windows.Forms.Button();
+            this.pctbxHeightmap = new System.Windows.Forms.PictureBox();
+            this.txtbxNumOfIterations = new System.Windows.Forms.TextBox();
+            this.lblNumOfIterations = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripMenuFile = new System.Windows.Forms.ToolStripDropDownButton();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cbxMapResolution = new System.Windows.Forms.ComboBox();
+            this.lblMapResolution = new System.Windows.Forms.Label();
+            this.txtbxTestOutput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbxHeightmap)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerateMap
+            // 
+            this.btnGenerateMap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerateMap.Location = new System.Drawing.Point(458, 297);
+            this.btnGenerateMap.Name = "btnGenerateMap";
+            this.btnGenerateMap.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerateMap.TabIndex = 0;
+            this.btnGenerateMap.Text = "Generate";
+            this.btnGenerateMap.UseVisualStyleBackColor = true;
+            this.btnGenerateMap.Click += new System.EventHandler(this.BtnGenerateMap_Click);
+            // 
+            // pctbxHeightmap
+            // 
+            this.pctbxHeightmap.Location = new System.Drawing.Point(12, 28);
+            this.pctbxHeightmap.Name = "pctbxHeightmap";
+            this.pctbxHeightmap.Size = new System.Drawing.Size(256, 256);
+            this.pctbxHeightmap.TabIndex = 1;
+            this.pctbxHeightmap.TabStop = false;
+            // 
+            // txtbxNumOfIterations
+            // 
+            this.txtbxNumOfIterations.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtbxNumOfIterations.Location = new System.Drawing.Point(458, 265);
+            this.txtbxNumOfIterations.Name = "txtbxNumOfIterations";
+            this.txtbxNumOfIterations.Size = new System.Drawing.Size(75, 20);
+            this.txtbxNumOfIterations.TabIndex = 2;
+            // 
+            // lblNumOfIterations
+            // 
+            this.lblNumOfIterations.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNumOfIterations.AutoSize = true;
+            this.lblNumOfIterations.Location = new System.Drawing.Point(350, 272);
+            this.lblNumOfIterations.Name = "lblNumOfIterations";
+            this.lblNumOfIterations.Size = new System.Drawing.Size(102, 13);
+            this.lblNumOfIterations.TabIndex = 3;
+            this.lblNumOfIterations.Text = "Number of Iterations";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuFile});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(545, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripMenuFile
+            // 
+            this.toolStripMenuFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripMenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem});
+            this.toolStripMenuFile.Image = ((System.Drawing.Image)(resources.GetObject("toolStripMenuFile.Image")));
+            this.toolStripMenuFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripMenuFile.Name = "toolStripMenuFile";
+            this.toolStripMenuFile.Size = new System.Drawing.Size(38, 22);
+            this.toolStripMenuFile.Text = "File";
+            this.toolStripMenuFile.ToolTipText = "File";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            // 
+            // cbxMapResolution
+            // 
+            this.cbxMapResolution.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxMapResolution.FormattingEnabled = true;
+            this.cbxMapResolution.Items.AddRange(new object[] {
+            "256x256",
+            "512x512",
+            "1024x1024",
+            "2048x2048"});
+            this.cbxMapResolution.Location = new System.Drawing.Point(458, 237);
+            this.cbxMapResolution.Name = "cbxMapResolution";
+            this.cbxMapResolution.Size = new System.Drawing.Size(75, 21);
+            this.cbxMapResolution.TabIndex = 5;
+            // 
+            // lblMapResolution
+            // 
+            this.lblMapResolution.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMapResolution.AutoSize = true;
+            this.lblMapResolution.Location = new System.Drawing.Point(346, 245);
+            this.lblMapResolution.Name = "lblMapResolution";
+            this.lblMapResolution.Size = new System.Drawing.Size(106, 13);
+            this.lblMapResolution.TabIndex = 6;
+            this.lblMapResolution.Text = "Heightmap resolution";
+            // 
+            // txtbxTestOutput
+            // 
+            this.txtbxTestOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtbxTestOutput.Location = new System.Drawing.Point(349, 28);
+            this.txtbxTestOutput.Multiline = true;
+            this.txtbxTestOutput.Name = "txtbxTestOutput";
+            this.txtbxTestOutput.Size = new System.Drawing.Size(184, 203);
+            this.txtbxTestOutput.TabIndex = 7;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 332);
+            this.Controls.Add(this.txtbxTestOutput);
+            this.Controls.Add(this.lblMapResolution);
+            this.Controls.Add(this.cbxMapResolution);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lblNumOfIterations);
+            this.Controls.Add(this.txtbxNumOfIterations);
+            this.Controls.Add(this.pctbxHeightmap);
+            this.Controls.Add(this.btnGenerateMap);
+            this.Name = "MainWindow";
+            this.Text = "Fault Line Heightmap Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pctbxHeightmap)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateMap;
+        private System.Windows.Forms.PictureBox pctbxHeightmap;
+        private System.Windows.Forms.TextBox txtbxNumOfIterations;
+        private System.Windows.Forms.Label lblNumOfIterations;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripMenuFile;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ComboBox cbxMapResolution;
+        private System.Windows.Forms.Label lblMapResolution;
+        private System.Windows.Forms.TextBox txtbxTestOutput;
+    }
+}
+
---
+++ Fault_Line_Algorithm/MainWindow.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fault_Line_Algorithm
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerateMap_Click(object sender, EventArgs e)
+        {
+            if (CheckInput(txtbxNumOfIterations.Text) == false) // Checks if input is correct
+            {
+                return;
+            }
+
+            int width = 0, height = 0;
+
+            switch (cbxMapResolution.SelectedIndex) // Defines the resolution of the Heightmap
+            {
+                case 0:
+                    pctbxHeightmap.Image = new Bitmap(256, 256);
+                    width = 256;
+                    height = 256;
+                    break;
+
+                case 1:
+                    pctbxHeightmap.Image = new Bitmap(512, 512);
+                    width = 512;
+                    height = 512;
+                    break;
+
+                case 2:
+                    pctbxHeightmap.Image = new Bitmap(1024, 1024);
+                    width = 1024;
+                    height = 1024;
+                    break;
+
+                case 3:
+                    pctbxHeightmap.Image = new Bitmap(2048, 2048);
+                    width = 2048;
+                    height = 2048;
+                    break;
+            }
+
+            using (Graphics blankMap = Graphics.FromImage(pctbxHeightmap.Image))
+            {
+                Rectangle mapSize = new Rectangle(0, 0, width, height);
+                blankMap.FillRectangle(Brushes.White, mapSize);
+            }
+
+            FaultFractal();
+        }
+
+
+        public void FaultFractal()
+        {
+            Stopwatch sw = new Stopwatch();
+
+            Bitmap heightMap = new Bitmap(pctbxHeightmap.Image); // Initializes the Heightmap
+            int itNum = Convert.ToInt32(txtbxNumOfIterations.Text); // Number of iterations
+            Random randomGenerator = new Random();
+
+            int x1 = 0, y1 = 0, x2 = 0, y2 = 0; // Coordinates for points
+            double m, b; // m - slope, b - y-intercept
+            double m1 = 0, b1 = 0; // m1 - slope, b1 - y-intercept, used for a fault calculated in the previous iteration
+            double yF; // Value for y coordinate on a fault line for given x
+            double yF1; // Value for y coordinate on a fault for given x, based on the previously generated fault
+
+            int h = 4; // Height displacement factor
+            
+
+            Color c; // Color object for storing RGB data of a pixel
+            int grey; // Value for calculating grey color
+
+            for (int j = 0; j < itNum; j++) // Generates itNum fault lines
+            {
+                // Calculates two random points, from which we will calculate a fault line
+
+                // It is less complex to simply not allow vertical lines to be generated
+
+                while (x1 == x2) // In this case, a vertical line would be the result
+                {
+                    x1 = randomGenerator.Next(0, heightMap.Width); // Generates the x coordinate of the first point
+                    x2 = randomGenerator.Next(0, heightMap.Width); // Generates the y coordinate of the second point
+                }
+
+                y1 = randomGenerator.Next(0, heightMap.Height); // Generates the y coordinate of the first point
+                y2 = randomGenerator.Next(0, heightMap.Height); // Generates the y coordinate of the second point
+
+                // Slope-intercept form of a line y = mx + b
+
+                m = (double)(y2 - y1) / (x2 - x1); // It calculates the slope m
+                b = (double)y1 - m * x1; // It calculates the y-intercept b
+                                         // Having m and b is enough to calculate the points on the fault line for comparison with the rest
+
+                txtbxTestOutput.AppendText("x1 = " + x1.ToString("F4") + " y1 = " + y1.ToString("F4")
+                    + " x2 =  " + x2.ToString("F4") + " y2 =  " + y2.ToString("F4") + "\n"
+                    + " k = " + m.ToString("F4") + " n = " + b.ToString("F4") + "\n");
+
+
+
+                for (int v = 0; v < heightMap.Height; v++) // Goes through the y coordinate of the bitmap
+                {
+                    for (int u = 0; u < heightMap.Width; u++) // Goes through the x coordinate of the bitmap
+                    {
+                        yF = m * u + b; // Calculates the yF (fault) coordinate of the current point (u, v)
+                        yF1 = m1 * u + b1; // Calculates the yF1 (previous fault) coordinates of the point (u, v)
+
+                        if (v >= yF)
+                        {
+                            c = heightMap.GetPixel(u, v);
+                            grey = c.R - h;
+
+                            if (grey > 0)
+                            {
+                                heightMap.SetPixel(u, v, Color.FromArgb(grey, grey, grey));
+                            }
+                        }
+                    }
+                }
+
+                m1 = m; // Sets the current m to act as a previous m in the next iteration
+                b1 = b; // Sets the current b to act as a previous b in the next iteration
+            }
+
+            pctbxHeightmap.Image = new Bitmap(heightMap);
+
+            pctbxHeightmap.Refresh();
+
+            sw.Stop();
+
+            MessageBox.Show("Time Elapsed: " + sw.Elapsed.ToString(), "Title",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public static bool CheckInput(string input) // Checks if input is valid in textBoxes
+        {
+            bool result = Int32.TryParse(input, out int number);
+
+            if (result)
+            {
+                if (number < 0)
+                {
+                    MessageBox.Show("Negative integers are not allowed.", "Error",
+                                     MessageBoxButtons.OK,
+                                     MessageBoxIcon.Error);
+
+                    return false;
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Error converting to integer.", "Error",
+                                     MessageBoxButtons.OK,
+                                     MessageBoxIcon.Error);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
